37bcdbfe481936daff2b9afc3e1d61720fda16aa
natan96 <natan96bsiuno.fipp@gmail.com>
2019-10-11 18:06:18 -0300

    Editar pontos

---
+++ src/pages/editar-pontos/editar-pontos.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the EditarPontosPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-editar-pontos',
+  templateUrl: 'editar-pontos.html',
+})
+export class EditarPontosPage {
+  add :boolean;
+  rem :boolean;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.add = true;
+    this.rem = false;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditarPontosPage');
+  }
+
+  updateAdd() {
+    this.rem = !this.add;
+  }
+
+  updateRem() {
+    this.add = !this.rem;
+  }
+
+}
